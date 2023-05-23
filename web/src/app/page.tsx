73c196d3aeeac98c1841482d4b16eeeac19cb271
Samanta Tessmer <samantartessmer@hotmail.com>
2023-05-23 18:57:44 -0300

    feat: adding to repository

---
+++ web/src/app/page.tsx
@@ -0,0 +1,5 @@
+import { EmptyMemories } from "@/components";
+
+export default function Home() {
+  return <EmptyMemories />;
+}
---
+++ web/src/components/index.tsx
@@ -0,0 +1,7 @@
+export { Copyright } from './Copyright';
+export { EmptyMemories } from './EmptyMemories';
+export { Hero } from './Hero';
+export { MediaPicker } from './MediaPicker';
+export { NewMemoryForm } from './NewMemoryForm';
+export { Profile } from './Profile';
+export { SignIn } from './SignIn';
